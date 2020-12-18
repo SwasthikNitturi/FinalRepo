9d47c0d5f60c97c0d7e2bba4da13aa7857822f60
swast <swast@LAPTOP-3B78J9BS>
2020-12-18 13:32:36 +0530

    newly added a class in new branch

---
+++ FinalProject/src/num/Area.java
@@ -0,0 +1,8 @@
+package num;
+
+public class Area {
+
+		public void area(long area) {
+			System.out.println("area");
+		}
+}
--- FinalProject/src/num/Numbers.java
+++ FinalProject/src/num/Numbers.java
@@ -1,13 +1,14 @@
 package num;
 
 public class Numbers {
-public void sysout() {
-	// TODO Auto-generated method stub
-	System.out.println("aef");
+	public void sysout() {
+		// TODO Auto-generated method stub
+		System.out.println("aef");
 
-}
-	public void sayHello(){
-		System.out.println("Hello Luck");	
 	}
-	
+
+	public void sayHello() {
+		System.out.println("Hello Luck");
+	}
+
 }
