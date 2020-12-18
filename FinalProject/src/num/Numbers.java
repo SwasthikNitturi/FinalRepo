c3645066ce52e44d5873bd5572e2160dd33c63e6
Swasthik <44065779+SwasthikNitturi@users.noreply.github.com>
2020-12-18 13:05:23 +0530

    Update Numbers.java
    added sayHello

--- FinalProject/src/num/Numbers.java
+++ FinalProject/src/num/Numbers.java
@@ -1,9 +1,13 @@
 package num;
 
 public class Numbers {
-private void sysout() {
+public void sysout() {
 	// TODO Auto-generated method stub
 	System.out.println("aef");
 
 }
+	public void sayHello(){
+		System.out.println("Hello Luck");	
+	}
+	
 }
