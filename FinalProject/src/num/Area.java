e11679a3b4a869dc43c8c3e7e163cdf023eba896
swast <swast@LAPTOP-3B78J9BS>
2020-12-18 15:31:37 +0530

    removed a method to raise conflit

--- FinalProject/src/num/Area.java
+++ FinalProject/src/num/Area.java
@@ -2,9 +2,6 @@ package num;
 
 public class Area {
 
-		public void area(long area) {
-			System.out.println("area");
-		}
 		public void product(int n1,int n2) {
 			System.out.println(n1*n2);
 		}
